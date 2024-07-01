285687c995ec8e259cb6f572fdc2850235afcb78
soerjo <ryohastomo@gmail.com>
2024-07-01 16:58:08 +0700

    update: inject config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get('DATABASE_URL'),
-      }),
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
       inject: [ConfigService],
     }),
   ],
